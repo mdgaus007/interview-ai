e52e5c39c288446797feb658ac6ac6c5a5282efc
MD GAUS <mdgauscse07@gmail.com>
2026-08-28 17:52:03 +0530

    Change API base URL to production server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://interview-ai-ixwy.onrender.com",
   withCredentials: true,
 });
 
