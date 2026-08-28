782a227b3d8637f3fc9dd4e804e201efd6b292b7
MD GAUS <mdgauscse07@gmail.com>
2026-08-28 18:15:46 +0530

    Update CORS origin for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://interview-ai-frontend-2115.onrender.com",
+    origin: "https://interview-ai-frontend01.onrender.com",
     credentials: true,
   }),
 );
